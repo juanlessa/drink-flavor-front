d90400556ec8738e2b4482e005c303af062e2bda
Juan Lessa <juanvlessa@ua.pt>
2023-05-06 21:23:11 +0100

    feat: :sparkles: landing page drinks grid

--- components/drinkCard/index.vue
+++ components/drinkCard/index.vue
@@ -3,23 +3,46 @@
 
         <img class="card-image" src="sex-on-beach.png" />
         <div class="card-content">
-            <h2 class="card-title">Sex on the beach</h2>
+            <div class="card-title">
+                <div>{{ props.drinkName }}</div>
+            </div>
             <div class="card-ingredients">
-                <div class="ingredient">
-                    <div class="ingredient-circle" :style="{ 'background-color': '#E077F1' }">
+                <div v-for="i in props.drinkIngredients" :key="i.name" class="ingredient">
+                    <div class="ingredient-circle" :style="{ 'background-color': i.colorTheme ? i.colorTheme : '#aabbcc' }">
                     </div>
                     <div class="ingredient-name">
-                        Vodka
+                        {{ i.name }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+
+export interface Ingredient {
+    name: string,
+    quantity: Number,
+    unity: string,
+    category: string,
+    isAlcoholic: boolean,
+    colorTheme?: string
+}
+const props = defineProps({
+    drinkName: {
+        type: String,
+        default: "",
+    },
+    drinkIngredients: {
+        type: Array<Ingredient>,
+        default: [],
+    },
+});
+</script>
 <style scoped>
 .card-container {
     background-color: var(--white);
-    height: 136px;
+    height: 120px;
     width: 100%;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     border-radius: 8px;
@@ -42,6 +65,10 @@
 .card-title {
     width: 100%;
     text-align: center;
+    height: 62px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
     font-style: normal;
     font-weight: 400;
@@ -50,8 +77,16 @@
 
 }
 
+.card-ingredients {
+    margin-top: 12px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 3px 7px;
+}
+
 .ingredient {
     display: flex;
+    align-items: center;
     gap: 4px;
 }
 
--- components/drinksSection/index.vue
+++ components/drinksSection/index.vue
@@ -1,16 +1,123 @@
 <template>
     <section class="section-container">
         <div class="section-content">
-            <DrinkCard />
-            <DrinkCard />
-            <DrinkCard />
-            <DrinkCard />
-            <DrinkCard />
+            <DrinkCard v-for="d in drinks" :key="d.id" :drink-name="d.name" :drink-ingredients="d.ingredients" />
 
         </div>
 
     </section>
 </template>
+<script setup lang="ts">
+const drinks = [
+    {
+        id: "1",
+        name: "Sex on the beach",
+        ingredients: [
+            {
+
+                name: "vodka",
+                quantity: 60,
+                unity: "ml",
+                category: "vodka",
+                isAlcoholic: true
+            },
+            {
+                name: "Peach liquor",
+                quantity: 45,
+                unity: "ml",
+                category: "liquor",
+                isAlcoholic: true
+            },
+            {
+                name: "Orange juice",
+                quantity: 150,
+                unity: "ml",
+                category: "juice",
+                isAlcoholic: false
+            }
+        ],
+        method: "bla bla bla",
+    },
+    {
+        id: "2",
+        name: "Strawberry caipirinhaaa",
+        ingredients: [
+            {
+                name: "Cachaça",
+                quantity: 60,
+                unity: "ml",
+                category: "cachaça",
+                isAlcoholic: true,
+                colorTheme: "#FC4040"
+            },
+            {
+                name: "Strawberry",
+                quantity: 3,
+                unity: "unity",
+                category: "fruit",
+                isAlcoholic: false,
+                colorTheme: "#346aaa"
+            },
+            {
+                name: "Lime juice",
+                quantity: 30,
+                unity: "ml",
+                category: "juice",
+                isAlcoholic: false,
+                colorTheme: "#9CFF94"
+            },
+            {
+                name: "Simple syrup",
+                quantity: 30,
+                unity: "ml",
+                category: "syrup",
+                isAlcoholic: false,
+                colorTheme: "#14213D"
+            }
+        ],
+        method: "bla bla bla",
+    },
+    {
+        id: "3",
+        name: "Mojito",
+        ingredients: [
+            {
+                name: "Rum",
+                quantity: 60,
+                unity: "ml",
+                category: "rum",
+                isAlcoholic: true
+            },
+            {
+                name: "Mint",
+                quantity: 8,
+                unity: "lift",
+                category: "lift",
+                isAlcoholic: false,
+                colorTheme: "#9CFF94"
+            },
+            {
+                name: "Lime juice",
+                quantity: 30,
+                unity: "ml",
+                category: "juice",
+                isAlcoholic: false,
+                colorTheme: "#9CFF94"
+            },
+            {
+                name: "Simple syrup",
+                quantity: 30,
+                unity: "ml",
+                category: "syrup",
+                isAlcoholic: false,
+                colorTheme: "#14213D"
+            }
+        ],
+        method: "bla bla bla",
+    }
+];
+
+</script>
 <style scoped>
 .section-container {
     width: 100vw;
