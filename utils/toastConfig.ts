f31d2d7482713e6ff606a4a1586427639a3fada8
Juan Lessa <juanvlessa@ua.pt>
2023-05-16 00:36:54 +0100

    feat: :sparkles: integrate toast notification

---
+++ utils/toastConfig.ts
@@ -0,0 +1,16 @@
+import { PluginOptions } from "vue-toastification";
+
+export default {
+    timeout: 2508,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: "button",
+    icon: true,
+    rtl: false
+     
+} as PluginOptions
