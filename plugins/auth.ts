accce609f9adb44fe1d59545f16020b6319b2e60
Juan Lessa <juanvlessa@ua.pt>
2023-07-11 14:30:39 +0100

    feat:

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,159 +1,149 @@
-import  { IAuthenticateResponse, ITokens, IRefreshTokenResponse } from '~/utils/dtos/Tokens'
-import { IUser } from '~/utils/dtos/Users'
+import { IAuthenticateResponse, ITokens, IRefreshTokenResponse } from "~/utils/dtos/Tokens";
+import { IUser } from "~/utils/dtos/Users";
 
 export type AuthState = {
 	user: {
 		email: string;
-        name: string
+		name: string;
 	};
 	authenticated: boolean;
 };
 
 const initState = (): AuthState => {
-    return {
-	    user: {
-		    email: '',
-            name: '',
-	    },
-	    authenticated: false
-    }
-}
+	return {
+		user: {
+			email: "",
+			name: "",
+		},
+		authenticated: false,
+	};
+};
 
 export default defineNuxtPlugin(() => {
+	const authState = useState<AuthState>("auth", initState);
 
-    const authState = useState<AuthState>('auth', initState);
+	const getAuthState = () => authState.value;
+	const resetAuthState = () => (authState.value = initState());
 
-    const getAuthState = () => authState.value;
-    const resetAuthState = () => authState.value = initState()
+	const storeTokens = ({ token, refresh_token }: ITokens) => {
+		// store refresh token
+		if (refresh_token.token) {
+			const refreshTokenCookie = useCookie("refresh_token", {
+				expires: refresh_token.expires,
+				maxAge: (refresh_token.expires.getTime() - new Date().getDate()) / 1000,
+			});
+			refreshTokenCookie.value = refresh_token.token;
+		}
 
-    const storeTokens = ({token, refresh_token}: ITokens) => {
-        // store refresh token
-        if(refresh_token.token){
-            const refreshTokenCookie = useCookie(
-                "refresh_token", 
-                {
-                    expires: refresh_token.expires,
-                    maxAge: (refresh_token.expires.getTime() - new Date().getDate())/1000
-                }
-            )
-            refreshTokenCookie.value = refresh_token.token
-        }
+		// store token
+		if (token.token) {
+			const tokenCookie = useCookie("token", {
+				expires: token.expires,
+				maxAge: (token.expires.getTime() - new Date().getTime()) / 1000,
+			});
+			tokenCookie.value = token.token;
+		}
+	};
 
-        // store token
-        if(token.token){
-            const tokenCookie = useCookie(
-                "token", 
-                {
-                    expires: token.expires,
-                    maxAge: (token.expires.getTime() - new Date().getTime())/1000
-                }
-            )
-            tokenCookie.value = token.token
-        }       
-    }
+	const checkTokens = () => {
+		const tokenCookie = useCookie("token");
+		const refreshTokenCookie = useCookie("refresh_token");
 
-    const checkTokens = () => {
-        const tokenCookie = useCookie('token')
-        const refreshTokenCookie = useCookie('refresh_token')
+		if (!tokenCookie.value && !refreshTokenCookie.value) {
+			return false;
+		}
+		return true;
+	};
 
+	const login = (data: IAuthenticateResponse) => {
+		const tokens: ITokens = {
+			token: {
+				token: data.token.token,
+				expires: new Date(data.token.expires),
+			},
+			refresh_token: {
+				token: data.refresh_token.token,
+				expires: new Date(data.refresh_token.expires),
+			},
+		};
 
-        if(!tokenCookie.value && !refreshTokenCookie.value){
-            return false;
-        }
-        return true;
+		storeTokens(tokens);
+		authState.value.authenticated = true;
+		authState.value.user.email = data.user.email;
+		authState.value.user.name = data.user.name;
+	};
 
-    }
+	const authenticate = async () => {
+		try {
+			const { $axios: axios } = useNuxtApp();
+			const { data } = await axios.get<IUser>("/users/profile");
 
-    const login = (data: IAuthenticateResponse) => {
-        const tokens: ITokens = {
-            token: {
-                token: data.token.token,
-                expires: new Date(data.token.expires),
-            },
-            refresh_token: {
-                token: data.refresh_token.token,
-                expires: new Date(data.refresh_token.expires),
-            },
-        };
+			authState.value.authenticated = true;
+			authState.value.user.email = data.email;
+			authState.value.user.name = data.name;
 
-        storeTokens(tokens);
-        authState.value.authenticated = true
-        authState.value.user.email = data.user.email
-        authState.value.user.name = data.user.name
-    }
+			return true;
+		} catch (error) {
+			console.error(error);
+			return false;
+		}
+	};
 
-    const authenticate = async() => {
-        try {
-            const { $axios: axios } = useNuxtApp()
-            const { data } = await axios.get<IUser>('/users/profile')
+	const signOut = (manual = false) => {
+		const refreshTokenCookie = useCookie("refresh_token");
+		refreshTokenCookie.value = undefined;
 
-            authState.value.authenticated = true
-            authState.value.user.email = data.email
-            authState.value.user.name = data.name
+		const tokenCookie = useCookie("token");
+		tokenCookie.value = undefined;
 
-            return true
-        }catch(error){
-            console.error(error)
-            return false
-        }
-    }
+		resetAuthState();
 
-    const signOut = (manual = false) => {
-        const refreshTokenCookie = useCookie("refresh_token")
-        refreshTokenCookie.value = undefined
+		if (manual) {
+			return navigateTo("/");
+		}
 
-        const tokenCookie = useCookie("token")
-        tokenCookie.value = undefined
+		return navigateTo("/login");
+	};
 
-        resetAuthState()
-        
-        if(manual){
-            return navigateTo("/")
-        }
+	const refreshToken = async () => {
+		const refreshTokenCookie = useCookie("refresh_token");
+		if (!refreshTokenCookie.value) {
+			return;
+		}
+		const { $axios: axios } = useNuxtApp();
+		try {
+			const { data } = await axios.post<IRefreshTokenResponse>(
+				"/refresh-token",
+				{ token: refreshTokenCookie.value },
+				{ headers: { NoAuth: true } }
+			);
+			const newAccessToken = data.token;
+			const newAccessTokenExpires = new Date(data.expires);
 
-        return navigateTo("/login")
-    }  
+			storeTokens({
+				refresh_token: { token: "", expires: {} as Date },
+				token: { token: newAccessToken, expires: newAccessTokenExpires },
+			});
 
-    const refreshToken = async() => {
-        const refreshTokenCookie = useCookie("refresh_token")
-        if(!refreshTokenCookie.value){
-            return;
-        }
-        const { $axios: axios } = useNuxtApp()
-        try {
-            const { data } = await axios.post<IRefreshTokenResponse>(
-                '/refresh-token',
-                { "token": refreshTokenCookie.value }, 
-                { headers: { NoAuth: true } }
-            )
-            const newAccessToken = data.token
-            const newAccessTokenExpires = new Date(data.expires)
-           
-            storeTokens({
-                refresh_token: { token: "", expires: {} as Date },
-                token: { token: newAccessToken, expires: newAccessTokenExpires }
-            })
- 
-            return newAccessToken
-            
-        } catch(error){
-            console.error('refresh token request error');
-            console.error(error)
+			return newAccessToken;
+		} catch (error) {
+			console.error("refresh token request error");
+			console.error(error);
 
-            signOut()
-            return;
-        }
-    }
+			signOut();
+			return;
+		}
+	};
 
-    return {
-        provide: {
-            getAuthState,
-            storeTokens,
-            signOut,
-            login,
-            refreshToken,
-            authenticate,
-            checkTokens,
-        },
-      };
-})
+	return {
+		provide: {
+			getAuthState,
+			storeTokens,
+			signOut,
+			login,
+			refreshToken,
+			authenticate,
+			checkTokens,
+		},
+	};
+});
--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,78 +1,79 @@
-import axios, { AxiosRequestHeaders, AxiosInstance, AxiosRequestConfig, AxiosResponse,InternalAxiosRequestConfig, AxiosError } from 'axios';
-import { DFAxiosError, DFAxiosErrorTypes } from '@/types/error';
-
+import axios, {
+	AxiosRequestHeaders,
+	AxiosInstance,
+	AxiosRequestConfig,
+	AxiosResponse,
+	InternalAxiosRequestConfig,
+	AxiosError,
+} from "axios";
+import { DFAxiosError, DFAxiosErrorTypes } from "@/types/error";
 
 export default defineNuxtPlugin(() => {
-    const { apiBase } = useRuntimeConfig().public
-    
-    const api = axios.create({
-		baseURL: apiBase
+	const { apiBase } = useRuntimeConfig().public;
+
+	const api = axios.create({
+		baseURL: apiBase,
 	});
 
-
-    const requestInterceptor = async(value: InternalAxiosRequestConfig<any>) => {
+	const requestInterceptor = async (value: InternalAxiosRequestConfig<any>) => {
 		if (value.headers?.NoAuth) {
-            return value;
-        }
+			return value;
+		}
 
-        const tokenCookie = useCookie("token")
-        if (!tokenCookie.value) {
-            return value
-        }
-        value.headers = {
+		const tokenCookie = useCookie("token");
+		if (!tokenCookie.value) {
+			return value;
+		}
+		value.headers = {
 			...value.headers,
-			Authorization: `Bearer ${tokenCookie.value}`
-		} as AxiosRequestHeaders
-        
-        return value
-    }
+			Authorization: `Bearer ${tokenCookie.value}`,
+		} as AxiosRequestHeaders;
 
-    const responseHandler = (response: AxiosResponse<string, unknown>) => {
-        return response;
-    };
+		return value;
+	};
 
+	const responseHandler = (response: AxiosResponse<string, unknown>) => {
+		return response;
+	};
 
-    const errorHandler = async(error: any) => {
-        if(!(error instanceof AxiosError)){
-            return Promise.reject(error);
-        }
-        const axiosError = error as DFAxiosError;
-        
-        if (axiosError.response && 
-            axiosError.response.status === 401 &&
-            axiosError.response.data.message === DFAxiosErrorTypes.ERR_MISSING_TOKEN 
-        ) {
+	const errorHandler = async (error: any) => {
+		if (!(error instanceof AxiosError)) {
+			return Promise.reject(error);
+		}
+		const axiosError = error as DFAxiosError;
 
-            const originalRequest = axiosError.config as InternalAxiosRequestConfig;
-            
-            try{
-                const newAccessToken = await useNuxtApp().$refreshToken();
-                if(!newAccessToken){
-                    return Promise.reject(error);
-                }
+		if (
+			axiosError.response &&
+			axiosError.response.status === 401 &&
+			axiosError.response.data.message === DFAxiosErrorTypes.ERR_MISSING_TOKEN
+		) {
+			const originalRequest = axiosError.config as InternalAxiosRequestConfig;
 
-                originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-                return axios(originalRequest);
-                
-            }catch(e){
-                return Promise.reject(error);
-            }
+			try {
+				const newAccessToken = await useNuxtApp().$refreshToken();
+				if (!newAccessToken) {
+					return Promise.reject(error);
+				}
+
+				originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+				return axios(originalRequest);
+			} catch (e) {
+				return Promise.reject(error);
+			}
 		}
 
 		return Promise.reject(error);
-
 	};
-    
 
 	api.interceptors.request.use((request) => requestInterceptor(request));
-    api.interceptors.response.use(
+	api.interceptors.response.use(
 		(response) => responseHandler(response),
 		(error) => errorHandler(error)
 	);
 
-    return {
-        provide: {
-            axios: api
-        },
-      };
-})
+	return {
+		provide: {
+			axios: api,
+		},
+	};
+});
