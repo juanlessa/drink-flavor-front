19ff44c0e841945b3e4e6face8ccddb13167ee02
Juan Lessa <juanvlessa@ua.pt>
2023-10-15 17:19:35 +0100

    feat: :sparkles: locale feature

--- app.vue
+++ app.vue
@@ -2,9 +2,21 @@
 	<NuxtPage />
 </template>
 <script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import { LANGUAGES } from "./types/translations";
+import { LOCAL_STORAGE_KEYS } from "./types/localStorage";
+import { THEME_MODES } from "./types/theme";
+
 const { $setTheme } = useNuxtApp();
+const { getLocalStorageItem } = useLocalStorage();
 
 onMounted(() => {
-	$setTheme();
+	const theme = (getLocalStorageItem(LOCAL_STORAGE_KEYS.theme) as THEME_MODES) ?? THEME_MODES.light;
+	$setTheme(theme);
 });
+
+if (process.client) {
+	const { locale } = useI18n();
+	locale.value = getLocalStorageItem(LOCAL_STORAGE_KEYS.locale) ?? LANGUAGES.english;
+}
 </script>
