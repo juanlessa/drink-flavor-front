363d6435e6d70d2a162519f890dceb69d2c91ffa
Juan Lessa <juanvlessa@ua.pt>
2023-10-15 17:21:35 +0100

    feat: :sparkles: use svg as component

---
+++ components/icons/menu.vue
@@ -0,0 +1,20 @@
+<template>
+	<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" :width="size" :height="size" viewBox="0 0 50 50">
+		<path
+			:fill="color"
+			d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"
+		></path>
+	</svg>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+	size: {
+		type: Number,
+		default: 128,
+	},
+	color: {
+		type: String,
+		default: "#14213D",
+	},
+});
+</script>
---
+++ components/icons/rightArrow.vue
@@ -0,0 +1,29 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		xmlns:xlink="http://www.w3.org/1999/xlink"
+		x="0px"
+		y="0px"
+		:width="size"
+		:height="size"
+		viewBox="0 0 96 96"
+		xml:space="preserve"
+	>
+		<path
+			d="M12,52h62.344L53.172,73.172c-1.562,1.562-1.562,4.094,0,5.656c1.562,1.562,4.095,1.562,5.657,0l28-28c1.562-1.562,1.562-4.095,0-5.656l-28-28C58.048,16.391,57.024,16,56,16c-1.023,0-2.047,0.391-2.828,1.172c-1.562,1.562-1.562,4.095,0,5.656L74.344,44H12c-2.209,0-4,1.791-4,4S9.791,52,12,52z"
+			:fill="color"
+		/>
+	</svg>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+	size: {
+		type: Number,
+		default: 128,
+	},
+	color: {
+		type: String,
+		default: "#14213D",
+	},
+});
+</script>
