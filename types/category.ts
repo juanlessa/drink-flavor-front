a4121b62dc943a11e21479e6fbec0b4ddf9c0b95
Juan Lessa <juanvlessa@ua.pt>
2023-10-18 03:45:12 +0100

    feat: :sparkles: category form types

--- types/category.ts
+++ types/category.ts
@@ -1,5 +1,6 @@
 import { DatabaseCommonInfo } from "@/types/database";
-import { ITranslations } from "@/types/translations";
+import { ITranslations, LANGUAGES } from "@/types/translations";
+import { FieldError } from "./forms";
 
 export type ICategoryTranslation = {
 	name: string;
@@ -13,10 +14,21 @@ export type ICreateCategory = {
 	translations: ITranslations<ICategoryTranslation>;
 };
 
+export type IUpdateCategory = {
+	id: string;
+	translations: ITranslations<ICategoryTranslation>;
+};
+
 export type CategoryState = {
 	categories: ICategory[];
 };
 
+export type ICategoryForm = {
+	translations: ITranslations<ICategoryTranslation>;
+};
+
 export type CategoryFormState = {
-	form: ICreateCategory;
+	form: ICategoryForm;
+	errors: FieldError[];
+	displayErrors: boolean;
 };
