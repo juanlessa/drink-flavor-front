dd180195b42c86750ebe080c952ccc322b2907de
Juan Lessa <juanvlessa@ua.pt>
2023-10-21 12:34:48 +0100

    feat: :sparkles: use ingredredient form

--- pages/ingredients/[id]/index.vue
+++ pages/ingredients/[id]/index.vue
@@ -1,28 +1,70 @@
 <template>
-	<PageTemplate bg-color="primary-background">
-		<IngredientsForm :ingredient="ingredient" class="main-content-padding" />
-	</PageTemplate>
+	<NuxtLayout :bg-color="IBgColor.primary">
+		<IngredientsForm class="main-content-padding" @submit="handleUpdateIngredient" />
+	</NuxtLayout>
 </template>
 <script setup lang="ts">
-import { Ingredient } from "@/utils/dtos/Ingredients";
-const { $axios: axios } = useNuxtApp();
+import { IBgColor } from "@/types/layout";
+import { IIngredient, IIngredientForm, IUpdateIngredient } from "@/types/ingredient";
+import { EMPTY_INGREDIENT } from "@/constants/ingredient";
+import { AxiosError } from "axios";
+
+const { $toast, $router } = useNuxtApp();
+const { getIngredient, updateIngredient } = useIngredient();
+const { getIngredientFormState, resetIngredientFormState } = useIngredientForm();
+
 const route = useRoute();
 
-const ingredient = ref<Ingredient>();
+const ingredient = ref<IIngredient>({ ...EMPTY_INGREDIENT });
+
+const ingredientFormState = getIngredientFormState();
+ingredientFormState.value.displayErrors = true;
 
 definePageMeta({
 	middleware: "auth",
 });
 
 onMounted(async () => {
-	const ingredientId = route.params.id;
+	const ingredientId = route.params.id as string;
 
 	try {
-		const { data } = await axios.get<Ingredient>(`/ingredients/${ingredientId}`);
-		ingredient.value = data;
+		ingredient.value = await getIngredient(ingredientId);
 	} catch (error) {
 		console.error(error);
+		return;
 	}
+
+	const ingredientForm: IIngredientForm = {
+		translations: ingredient.value.translations,
+		is_alcoholic: ingredient.value.is_alcoholic,
+		category_id: ingredient.value.category._id,
+	};
+	ingredientFormState.value.form = ingredientForm;
 });
+
+const handleUpdateIngredient = async () => {
+	const ingredientToUpdate: IUpdateIngredient = {
+		id: ingredient.value._id,
+		...ingredientFormState.value.form,
+	};
+
+	try {
+		await updateIngredient(ingredientToUpdate);
+	} catch (error) {
+		const axiosError = error as AxiosError;
+		console.error(error);
+		if (axiosError.response?.status === 400) {
+			const errorMessage = (axiosError.response.data as { status: string; message: string }).message;
+			$toast.error(errorMessage);
+		}
+		return;
+	}
+
+	$toast.success("SUCCESS");
+	setTimeout(() => {
+		$router.back();
+		resetIngredientFormState();
+	}, 750);
+	return;
+};
 </script>
-<style scoped></style>
--- pages/ingredients/new.vue
+++ pages/ingredients/new.vue
@@ -1,10 +1,41 @@
 <template>
-	<PageTemplate bg-color="primary-background">
-		<IngredientsForm class="main-content-padding" />
-	</PageTemplate>
+	<NuxtLayout :bg-color="IBgColor.primary">
+		<IngredientsForm class="main-content-padding" @submit="handleCreateIngredient" />
+	</NuxtLayout>
 </template>
 <script setup lang="ts">
+import { IBgColor } from "@/types/layout";
+import { AxiosError } from "axios";
+
+const { $toast, $router } = useNuxtApp();
+const { createIngredient } = useIngredient();
+const { getIngredientFormState, resetIngredientFormState } = useIngredientForm();
+
+const ingredientFormState = getIngredientFormState();
+
 definePageMeta({
 	middleware: "auth",
 });
+
+const handleCreateIngredient = async () => {
+	const ingredientToCreate = ingredientFormState.value.form;
+	try {
+		await createIngredient(ingredientToCreate);
+	} catch (error) {
+		const axiosError = error as AxiosError;
+		console.error(error);
+		if (axiosError.response?.status === 400) {
+			const errorMessage = (axiosError.response.data as { status: string; message: string }).message;
+			$toast.error(errorMessage);
+		}
+		return;
+	}
+
+	$toast.success("SUCCESS");
+	setTimeout(() => {
+		$router.back();
+		resetIngredientFormState();
+	}, 750);
+	return;
+};
 </script>
