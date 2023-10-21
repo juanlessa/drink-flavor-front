816df0138a89c53cddfaee3113bf0dcb7bf54aef
Juan Lessa <juanvlessa@ua.pt>
2023-10-21 12:29:47 +0100

    refactor: :recycle: change set theme call

--- app.vue
+++ app.vue
@@ -10,12 +10,10 @@ import { THEME_MODES } from "./types/theme";
 const { $setTheme } = useNuxtApp();
 const { getLocalStorageItem } = useLocalStorage();
 
-onMounted(() => {
+if (process.client) {
 	const theme = (getLocalStorageItem(LOCAL_STORAGE_KEYS.theme) as THEME_MODES) ?? THEME_MODES.light;
 	$setTheme(theme);
-});
 
-if (process.client) {
 	const { locale } = useI18n();
 	locale.value = getLocalStorageItem(LOCAL_STORAGE_KEYS.locale) ?? LANGUAGES.english;
 }
