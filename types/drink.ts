8c55b39ebece51e733eab2793e0a91504499241a
Juan Lessa <juanvlessa@ua.pt>
2023-10-21 20:25:41 +0100

    feat: :sparkles: drink composable types

--- types/drink.ts
+++ types/drink.ts
@@ -1,6 +1,7 @@
 import { DatabaseCommonInfo } from "@/types/database";
 import { IIngredient } from "@/types/ingredient";
 import { ITranslations } from "@/types/translations";
+import { FieldError } from "@/types/forms";
 
 export type IDrinkTranslation = {
 	name: string;
@@ -21,4 +22,21 @@ export type IDrink = DatabaseCommonInfo & {
 
 export type DrinkState = {
 	drinks: IDrink[];
+};
+
+export type IDrinkIngredientRequest = {
+	quantity: number;
+	ingredient_id: string;
+};
+
+export type IDrinkForm = {
+	translations: ITranslations<IDrinkTranslation>;
+	ingredients: IDrinkIngredientRequest[];
+};
+
+export type DrinkFormState = {
+	form: IDrinkForm;
+	errors: FieldError[];
+	displayErrors: boolean;
+	defaultIngredients: IDrinkIngredientRequest[];
 };
