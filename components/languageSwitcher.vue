b68c9cba2195955537b2aba59640e54ac043c56e
Juan Lessa <juanvlessa@ua.pt>
2023-10-29 00:34:06 +0100

    refactor: :recycle: use radix select component

--- components/languageSwitcher.vue
+++ components/languageSwitcher.vue
@@ -1,20 +1,65 @@
 <template>
-	<div class="language-switcher">
-		<select v-model="$i18n.locale" class="language-select-input" id="language">
-			<option v-for="lang in languages" :key="lang" :value="lang">
-				{{ lang.toLocaleUpperCase() }}
-			</option>
-		</select>
-	</div>
+	<SelectRoot v-model="$i18n.locale">
+		<SelectTrigger
+			class="inline-flex items-center justify-between rounded pl-[0.5rem] text-[1rem] leading-none h-[1.5rem] gap-[0.25rem] text-grass11 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-green9 outline-none"
+		>
+			<SelectValue />
+			<IconsBottomArrow :size="22" :color="themeState.colors.iconsColor" />
+		</SelectTrigger>
+
+		<SelectPortal>
+			<SelectContent
+				class="min-w-[4.5rem] bg-primary rounded shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade z-[100]"
+				position="popper"
+				:sideOffset="-25"
+				align="end"
+			>
+				<SelectViewport class="p-[0.25rem]">
+					<SelectGroup>
+						<SelectItem
+							v-for="(option, index) in languages"
+							:key="index"
+							class="leading-none text-grass11 rounded-md flex items-center h-[1.5rem] pl-[1.5rem] pr-[0.5rem] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-secondary data-[highlighted]:text-green1"
+							:value="option"
+						>
+							<SelectItemIndicator
+								class="absolute left-0 w-[25px] inline-flex items-center justify-center"
+							>
+								<IconsChecked :size="10" :color="themeState.colors.iconsColor" />
+							</SelectItemIndicator>
+							<SelectItemText>
+								{{ option.toLocaleUpperCase() }}
+							</SelectItemText>
+						</SelectItem>
+					</SelectGroup>
+				</SelectViewport>
+			</SelectContent>
+		</SelectPortal>
+	</SelectRoot>
 </template>
 <script setup lang="ts">
+import {
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectItemIndicator,
+	SelectItemText,
+	SelectPortal,
+	SelectRoot,
+	SelectTrigger,
+	SelectValue,
+	SelectViewport,
+} from "radix-vue";
+
 import { useI18n } from "vue-i18n";
 import { LANGUAGES } from "@/types/translations";
 import { LOCAL_STORAGE_KEYS } from "@/types/localStorage";
 
+const { $getTheme } = useNuxtApp();
 const { locale } = useI18n();
 const { setLocalStorageItem } = useLocalStorage();
 
+const themeState = $getTheme();
 const languages = Object.values(LANGUAGES);
 
 watch(locale, (newLocale) => {
