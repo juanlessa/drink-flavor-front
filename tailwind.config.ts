5e28bf3c068d00e393b88010cab0a4f1f15a2ed1
Juan Lessa <juanvlessa@ua.pt>
2023-11-06 17:21:59 +0000

    refactor: :recycle: improve colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ module.exports = {
 	theme: {
 		extend: {},
 		colors: {
+			transparent: "transparent",
+			white: "#FFFFFF",
+			txt: {
+				dark: "#14213D",
+				light: "#FFFFFF",
+			},
+			gray: {
+				10: "#f0f0f0",
+				40: "#f4f4f4",
+				50: "#f9f9f9",
+				100: "#f2f2f2",
+				200: "#e5e6e8",
+				300: "#c5cc c3",
+				400: "#8b91 9a",
+				450: "#6b72 80",
+				500: "#595959",
+				600: "#16210c",
+				light: "#e9e9e9",
+			},
 			light: {
 				theme: "#4E86E4",
 				primary: "#FFFFFF",
@@ -22,23 +41,6 @@ module.exports = {
 				theme: "#FFD700",
 				primary: "#262528",
 				secondary: "#3F3F41",
-			},
-
-			transparent: "transparent",
-
-			white: "#FFFFFF",
-			gray: {
-				10: "#f0f0f0",
-				40: "#f4f4f4",
-				50: "#f9f9f9",
-				100: "#f2f2f2",
-				200: "#e5e6e8",
-				300: "#c5ccc3",
-				400: "#8b919a",
-				450: "#6b7280",
-				500: "#595959",
-				600: "#16210c",
-				light: "#e9e9e9",
 			},
 		},
 	},
